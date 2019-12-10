ea9ea64fb7f702c60145f57bb5a6ba35ae065d20
servando.reyes <servando.reyes@etermax.com>
2019-12-10 12:41:29 -0300

    Tests finished

--- magma_ser_nicocho/src/main/kotlin/PurchaseTransaction.kt
+++ magma_ser_nicocho/src/main/kotlin/PurchaseTransaction.kt
@@ -3,10 +3,34 @@ package main.kotlin
 class PurchaseTransaction(private val purchase: Purchase) {
     operator fun invoke(): Invoice {
         val discount = calculateDiscount()
-        return Invoice(8F, 0F)
+        val total = calculateTotal(discount)
+        return Invoice(total, discount)
     }
 
     private fun calculateDiscount(): Float {
-        purchase.basket.filter { book -> book.volume.value % 2L == 0 }
+        return when(checkDifferentVolumes()){
+            1 -> 0.05F
+            2 -> 0.1F
+            3 -> 0.15F
+            4 -> 0.2F
+            else -> 0F
+        }
+    }
+
+    private fun calculateTotal(discount: Float): Float {
+        var total = 0L
+        purchase.basket.map { total += it.cost }
+        return total - (total * discount)
+    }
+
+    private fun checkDifferentVolumes(): Int {
+        var different = mutableListOf<Book>()
+        purchase.basket.sortedBy { book -> book.volume.value }
+        purchase.basket.forEachIndexed { index, book ->
+            if ( (index+1)<purchase.basket.size && book.volume.value != purchase.basket[index+1].volume.value){
+                different.add(book)
+            }
+        }
+        return different.size
     }
 }
--- magma_ser_nicocho/src/test/kotlin/PurchaseBookTest.kt
+++ magma_ser_nicocho/src/test/kotlin/PurchaseBookTest.kt
@@ -19,33 +19,77 @@ class PurchaseBookTest {
     fun `check book was created`() {
         val volume = Volume(1)
         val book = Book(8, volume)
-        Assert.assertEquals(8, book.cost)
+        Assert.assertEquals(BOOK_COST, book.cost)
         Assert.assertEquals(1, book.volume.value)
     }
 
     @Test
     fun `when one book is buyed doesn't have discount`() {
         val volume = Volume(1)
-        val book = Book(8, volume)
+        val book = Book(BOOK_COST, volume)
 
         val purchase = Purchase(listOf(book))
         val action = PurchaseTransaction(purchase)
 
         val invoice = action()
 
-        Assert.assertEquals(0F, invoice.discount)
+        Assert.assertEquals(NO_PERCENT_DISCOUNT_TOTAL, invoice.total)
     }
 
     @Test
-    fun `when 2 books are buyed doesn't have discount`() {
+    fun `when 2 books are buyed have 5 percent discount`() {
         val volume = Volume(1)
-        val book = Book(8, volume)
-
-        val purchase = Purchase(listOf(book, book))
+        val volume2 = Volume(2)
+        val book = Book(BOOK_COST, volume)
+        val book2 = Book(BOOK_COST, volume2)
+        val purchase = Purchase(listOf(book, book2))
         val action = PurchaseTransaction(purchase)
 
         val invoice = action()
 
-        Assert.assertEquals(5F, invoice.discount)
+        Assert.assertEquals(FIVE_PERCENT_DISCOUNT_TOTAL, invoice.total)
+    }
+
+    @Test
+    fun `when 3 books are buyed have 10 percent discount`() {
+        val volume = Volume(1)
+        val volume2 = Volume(2)
+        val volume3 = Volume(3)
+        val book = Book(BOOK_COST, volume)
+        val book2 = Book(BOOK_COST, volume2)
+        val book3 = Book(BOOK_COST, volume3)
+        val purchase = Purchase(listOf(book, book2, book3))
+        val action = PurchaseTransaction(purchase)
+
+        val invoice = action()
+
+        Assert.assertEquals(TEN_PERCENT_DISCOUNT_TOTAL, invoice.total)
+    }
+
+
+    @Test
+    fun `when 4 books are buyed have 15 percent discount`() {
+        val volume = Volume(1)
+        val volume2 = Volume(2)
+        val volume3 = Volume(3)
+        val volume4 = Volume(4)
+        val book = Book(BOOK_COST, volume)
+        val book2 = Book(BOOK_COST, volume2)
+        val book3 = Book(BOOK_COST, volume3)
+        val book4 = Book(BOOK_COST, volume4)
+        val purchase = Purchase(listOf(book, book2, book3, book4))
+        val action = PurchaseTransaction(purchase)
+
+        val invoice = action()
+
+        Assert.assertEquals(FIVETEEN_PERCENT_DISCOUNT_TOTAL, invoice.total)
+    }
+
+    companion object {
+        val BOOK_COST = 8L
+        val NO_PERCENT_DISCOUNT_TOTAL = 8.0F
+        val FIVE_PERCENT_DISCOUNT_TOTAL = 15.2F
+        val TEN_PERCENT_DISCOUNT_TOTAL = 21.6F
+        val FIVETEEN_PERCENT_DISCOUNT_TOTAL = 27.2F
     }
 }
